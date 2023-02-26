50113c32736059956644ebdb7d89cb789308f2cf
Sagar <86335892+Th-Sagar@users.noreply.github.com>
2023-02-26 21:24:48 +0545

    new

--- shortest_job_first_scheduling.c
+++ shortest_job_first_scheduling.c
@@ -2,44 +2,44 @@
 
 int main()
 {
-    int arrival_time[10], burst_time[10], temp[10];
-    int i, smallest, count = 0, time, limit;
-    double wait_time = 0, turnaround_time = 0, end;
-    float average_waiting_time, average_turnaround_time;
+    int at[10], bt[10], temp[10];
+    int i, sm, count = 0, time, limit;
+    double wt = 0, tat = 0, end;
+    float awt, avt;
     printf("\nEnter the Total Number of Processes:\t");
     scanf("%d", &limit);
-    printf("\nEnter Details of %d Processesn", limit);
+    printf("\nEnter Details of %d Processes\n", limit);
     for (i = 0; i < limit; i++)
     {
         printf("\nEnter Arrival Time:\t");
-        scanf("%d", &arrival_time[i]);
+        scanf("%d", &at[i]);
         printf("Enter Burst Time:\t");
-        scanf("%d", &burst_time[i]);
-        temp[i] = burst_time[i];
+        scanf("%d", &bt[i]);
+        temp[i] = bt[i];
     }
-    burst_time[9] = 9999;
+    bt[9] = 9999;
     for (time = 0; count != limit; time++)
     {
-        smallest = 9;
+        sm = 9;
         for (i = 0; i < limit; i++)
         {
-            if (arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+            if (at[i] <= time && bt[i] < bt[sm] && bt[i] > 0)
             {
-                smallest = i;
+                sm = i;
             }
         }
-        burst_time[smallest]--;
-        if (burst_time[smallest] == 0)
+        bt[sm]--;
+        if (bt[sm] == 0)
         {
             count++;
             end = time + 1;
-            wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
-            turnaround_time = turnaround_time + end - arrival_time[smallest];
+            wt = wt + end - at[sm] - temp[sm];
+            tat = tat + end - at[sm];
         }
     }
-    average_waiting_time = wait_time / limit;
-    average_turnaround_time = turnaround_time / limit;
-    printf("\n\nAverage Waiting Time:\t%lf\n", average_waiting_time);
-    printf("Average Turnaround Time:\t%lf\n", average_turnaround_time);
+    awt = wt / limit;
+    avt = tat / limit;
+    printf("\n\nAverage Waiting Time:\t%lf\n", awt);
+    printf("Average Turnaround Time:\t%lf\n", avt);
     return 0;
 }
